b5b952d72b5e7679f4e21b366908f9dd2b5a5426
miaoruiyuan <miao201110@gmail.com>
2024-08-01 11:50:51 +0800

    修改 package.swift

--- Sources/MFSIdentifier/MFSIdentifier-Bridging-Header.h
+++ Sources/MFSIdentifier/MFSIdentifier-Bridging-Header.h
@@ -8,20 +8,20 @@
 #ifndef Header_h
 #define Header_h
 
-//#import "../MFSIdentifierObjC/MFSCache/MFSCacheManager/MFSCacheManager.h"
-//#import "../MFSIdentifierObjC/MFSIdentifier.h"
-//
-//
-////#import "../MFSIdentifierObjC/MFSSafariIDManager.h"
-////#import "../MFSIdentifierObjC/MFSCache/MFSCache.h"
-//
-//#import "../MFSIdentifierObjC/MFSCache/MFSCacheManager/MFSCacheStorage.h"
-//#import "../MFSIdentifierObjC/MFSCache/MFSCacheManager/MFSCacheStorageObject.h"
-//
-//#import "../MFSIdentifierObjC/MFSCache/AuxCategory/NSString+MFSEncrypt.h"
-//#import "../MFSIdentifierObjC/MFSCache/AuxCategory/NSData+MFSAES.h"
-//#import "../MFSIdentifierObjC/MFSCache/AuxCategory/NSJSONSerialization+MFSJSONString.h"
-//#import "../MFSIdentifierObjC/MFSJSONEntity/MFSJSONEntity.h"
-//#import "../MFSIdentifierObjC/MFSJSONEntity/NSObject+JSONEntity.h"
+#import "../MFSIdentifierObjC/MFSCache/MFSCacheManager/MFSCacheManager.h"
+#import "../MFSIdentifierObjC/MFSIdentifier.h"
+
+
+//#import "../MFSIdentifierObjC/MFSSafariIDManager.h"
+//#import "../MFSIdentifierObjC/MFSCache/MFSCache.h"
+
+#import "../MFSIdentifierObjC/MFSCache/MFSCacheManager/MFSCacheStorage.h"
+#import "../MFSIdentifierObjC/MFSCache/MFSCacheManager/MFSCacheStorageObject.h"
+
+#import "../MFSIdentifierObjC/MFSCache/AuxCategory/NSString+MFSEncrypt.h"
+#import "../MFSIdentifierObjC/MFSCache/AuxCategory/NSData+MFSAES.h"
+#import "../MFSIdentifierObjC/MFSCache/AuxCategory/NSJSONSerialization+MFSJSONString.h"
+#import "../MFSIdentifierObjC/MFSJSONEntity/MFSJSONEntity.h"
+#import "../MFSIdentifierObjC/MFSJSONEntity/NSObject+JSONEntity.h"
 
 #endif /* Header_h */
