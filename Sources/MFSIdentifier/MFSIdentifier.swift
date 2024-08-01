99da8dd78623f1ec842b85caf2632ee26350b931
miaoruiyuan <miao201110@gmail.com>
2024-08-01 14:46:23 +0800

    create MFSIdentifierSwift

--- Sources/MFSIdentifier/MFSIdentifier.swift
+++ Sources/MFSIdentifier/MFSIdentifier.swift
@@ -1,4 +1,14 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
+import Foundation
+import MFSIdentifierObjC
+
+public struct MFSIdentifierSwift {
+    public static func deviceID() -> String {
+       return MFSIdentifier.deviceID()
+    }
+}
+
+
 
